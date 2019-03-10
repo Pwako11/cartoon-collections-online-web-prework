56caee4f3251f75a3fe197ad9d7f25e4478dc051
Patrick A <akolo@hotmail.com>
2019-03-10 18:34:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarves)
     #dwarves = ["Dopey", "Grumpy", "Bashful"]
+<<<<<<< HEAD
     dwarves.each.with_index {|dwarves, index| puts "#{index +1}. #{dwarves}"}
 end
 
@@ -7,6 +8,16 @@ def summon_captain_planet(veggies, fruits)
   veggies.collect { |x| x.capitalize + "!" }
   fruits.collect { |x| x.capitalize + "!" }
   end
+=======
+    dwarves.each.with_index(1) {|val, ind| puts /"#{ind}. #{val}"/}
+end
+
+def summon_captain_planet (veggies, fruits)
+  veggies
+  fruits
+
+end
+>>>>>>> 8f4a61cf5b9a1d641136db644d3499f25ca1764c
 
 def long_planeteer_calls# code an argument here
   # Your code here
